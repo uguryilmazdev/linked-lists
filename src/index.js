f9d94e83c879f658d5d4d669c4791538dcdd2051
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-13 15:29:40 +0300

    Create linkedlist class and append method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 class Node {
   constructor(value = null, nextNode = null) {
     this.value = value;
-    this.nextNode = nextNode;
+    this.next = nextNode;
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode; // add after tail
+      this.tail = newNode; // update linkedlist tail
+    }
+  }
+}
