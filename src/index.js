57d3b8bc7c0b5e8b21b6652d83da8adbe8d59000
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-13 14:54:12 +0300

    Complete node class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+class Node {
+  constructor(value) {
+    this.value = value || null;
+    this.nextNode = null;
+  }
 
+  changeValue(newValue) {
+    this.value = newValue;
+  }
+}
+
+class LinkedList {
+  append(value) {}
+
+  prepend(value) {}
+}
